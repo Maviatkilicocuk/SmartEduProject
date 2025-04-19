ec5a95fc3dc6a44686d616048e8f2b8d98e6ba68
Enver Bilal Birinci <enver.birinci@inavitas.com>
2025-04-19 20:43:31 +0300

    Ikinci commit

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const express = require('express');
 
 const app = express();
 
+// Template Engine
+app.set('view engine', 'ejs');
+
+//Middleware
+app.use(express.static('public'));
+
+//Routes
 app.get('/', (req, res) => {
-  res.send('Index Sayfasi');
+  res.status(200).render('index', {
+    page_name:"index"
+  });
 });
+
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+      page_name:"about"
+    });
+  });
 
 const port = 3000;
 app.listen(port, () => {
