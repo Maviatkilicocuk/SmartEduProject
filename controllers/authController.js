6ccd1acf3428c63fe961b607a637ab17fc5816f4
Enver Bilal Birinci <enver.birinci@inavitas.com>
2025-04-25 00:04:09 +0300

    Kullanıcı tanımlamaları

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,11 +5,7 @@ exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
 
-    res.status(201).json({
-      page_name: 'user',
-      status: 'success',
-      user,
-    });
+    res.status(201).redirect('/login');
   } catch (err) {
     res.status(400).json({
       status: 'fail',
